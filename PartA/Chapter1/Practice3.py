152b70e02587c435c51e8ea2e4221d2c0f4f2568
York <ysmintor@gmail.com>
2018-03-25 20:19:24 +0800

    add practice 3

---
+++ PartA/Chapter1/Practice3.py
@@ -0,0 +1,13 @@
+a = 'abcd'
+print(a[-1])
+print(a[3])
+
+b = 'jay'
+c = 'python'
+
+template = "my name is %s , i love %s" % ( b, c)
+myapple = "this is %s %s" % ("my", "apple")
+
+
+print(template)
+print(myapple)
